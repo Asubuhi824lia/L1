3ca5e41809d90e5f6f4489be1d93570fe7fdb671
Asubuhi824lia <der_anastasi84@mail.ru>
2023-11-05 16:46:31 +0700

    change: rename html titles

--- 7_funcArray/index.js
+++ 7_funcArray/index.js
@@ -4,6 +4,7 @@ const callFuncs = [
     fib(19),
     fibNums(14)
 ]
+console.log('\n\nWith init values in array')
 callFuncs.forEach((func, index) => console.log(`${index}: `, func))
 
 
@@ -19,5 +20,5 @@ funcs.forEach((func, index) => console.log(`${index}: `, func.call(this, 15)))
 console.log('\n\napply')
 funcs.forEach((func, index) => console.log(`${index}: `, func.apply(this, [15])))
 
-console.log('\n\nbind')
+console.log('\n\nWithout init values in array')
 funcs.forEach((func, index) => {func.bind(this); console.log(`${index}: `, func(14))})
